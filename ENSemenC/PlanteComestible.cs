4874d043ad84428ea2c0f991b8a43b0ac72c6e15
Hugo Cauzid <hcauzid@hotmail.fr>
2025-04-28 18:31:55 +0200

    Déplacement fonction Grandir dans Plante, ajout variable aFruit dans Plante Comestible

--- ENSemenC/PlanteComestible.cs
+++ ENSemenC/PlanteComestible.cs
@@ -1,5 +1,14 @@
 public abstract class PlanteComestible : PlanteCommercialisable
 {
+    public bool aFruit { get; set; }
     public PlanteComestible(string nom, Saison saisonPreferee, Terrain terrain, int espacement, int place, double besoinsEauMin, double besoinsEauMax, double besoinsLuminositeMin, double besoinsLuminositeMax, int vitesseDeCroissance, int esperanceDeVie, int croissanceMin, decimal prix, string nature = "Comestible") : base(nom, saisonPreferee, terrain, espacement, place, besoinsEauMin, besoinsEauMax, besoinsLuminositeMin, besoinsLuminositeMax, vitesseDeCroissance, esperanceDeVie, croissanceMin, prix, nature)
-    { }
+    {
+        aFruit = false;
+    }
+
+    public override void Grandir(int nbVoisins)
+    {
+        base.Grandir(nbVoisins);
+        // appel fonction pour fruits
+    }
 }
--- ENSemenC/obj/Debug/net8.0/ENSemenC.AssemblyInfo.cs
+++ ENSemenC/obj/Debug/net8.0/ENSemenC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ENSemenC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b59e62aab65f1c1919b3d0afa9164463ee670a96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df68453740dda37cf2085f1731eca56d6df78814")]
 [assembly: System.Reflection.AssemblyProductAttribute("ENSemenC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ENSemenC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
