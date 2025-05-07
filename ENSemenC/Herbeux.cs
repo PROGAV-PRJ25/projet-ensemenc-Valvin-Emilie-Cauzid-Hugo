ffcc16603b7a90421ddca2fcdf306eefafa2d03a
emilievln <emilie.valvin@gmail.com>
2025-05-07 10:44:13 +0200

    correction def terrain

--- ENSemenC/Herbeux.cs
+++ ENSemenC/Herbeux.cs
@@ -1,5 +1,5 @@
 public class Herbeux : Terrain
 {
-    public Herbeux(Terrain terrain) : base("Herbeux", 0.5, new double[][] { new double[] { 12.1, 18.0, 25.0, 12.5, 10.0, 12.0, 0.0 }, new double[] { 20.4, 30.0, 45.0, 22.0, 19.5, 28.0, 0.0 }, new double[] { 13.4, 25.0, 30.0, 15.0, 16.5, 20.0, 0.0 }, new double[] { 0.7, 10.0, 20.0, 5.0, 8.0, 12.0, 0.0 }, })
+    public Herbeux() : base("Herbeux", 0.5, new double[][] { new double[] { 12.1, 18.0, 25.0, 12.5, 10.0, 12.0, 0.0 }, new double[] { 20.4, 30.0, 45.0, 22.0, 19.5, 28.0, 0.0 }, new double[] { 13.4, 25.0, 30.0, 15.0, 16.5, 20.0, 0.0 }, new double[] { 0.7, 10.0, 20.0, 5.0, 8.0, 12.0, 0.0 }, })
     { }
 }
