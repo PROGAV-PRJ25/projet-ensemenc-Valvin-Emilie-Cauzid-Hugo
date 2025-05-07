88b65f597b32e74b2ca48e0d24fb69dd0335a02c
Hugo Cauzid <hcauzid@hotmail.fr>
2025-05-07 10:40:38 +0200

    Légers changements

--- ENSemenC/Program.cs
+++ ENSemenC/Program.cs
@@ -1,1 +1,11 @@
-//créer des terrains
+﻿// Créer des terrains (penser à la météo et la saison associées)
+
+// Vérifier que la MAJ de la luminosité et de l'humidité marchent bien
+
+// Créer des plantes
+
+// Associer les 2
+
+// Vérifier que les fonctions pour chercher luminosité et humidité marchent bien
+
+// Vérifier que la croissance de la plante fonctionne (long à faire je pense)
