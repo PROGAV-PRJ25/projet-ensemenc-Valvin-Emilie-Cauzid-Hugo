f9cc61210bfd1d9d54f1ffaed499a41a2f798b9c
emilievln <emilie.valvin@gmail.com>
2025-05-07 10:10:19 +0200

    définition base température sur chaque terrain

--- ENSemenC/Jungle.cs
+++ ENSemenC/Jungle.cs
@@ -1,5 +1,5 @@
 public class Jungle : Terrain
 {
-    public Jungle(Terrain terrain) : base("Jungle", 0.75)
+    public Jungle(Terrain terrain) : base("Jungle", 0.75, {new double[]{13.1, 19.0, 27.0, 13.4, 9.8, 12.4, 0.0}, new double[] {21.4, 33.0, 47.0, 22.5, 19.8, 28.7, 0.0}, new double[] {13.4, 28.0, 31.0, 17.0, 16.8, 21.0, 0.0}, new double[] {0.2, 9.5,18.7,5.3,8.4,11.0,0.0},})
     { }
 }
